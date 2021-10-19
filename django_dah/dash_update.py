aca5662bd5418a0470624313faf627434fa14a76
goel-prakhar <prakhar.goel@affinsys.com>
2021-10-19 11:49:44 +0530

    added dash_update.py
    dash_update file contains table with time  filter and with download csv option

--- django_dah/dash_update.py
+++ django_dah/dash_update.py
@@ -28,94 +28,94 @@ def new_dash(**kwargs):
 	app = DjangoDash('dash_integration_id')
 
 	app.css.append_css({
-    "external_url": external_stylesheets
-    })
-    
-    app.layout = html.Div(
-    [
+	"external_url": external_stylesheets
+	})
+	
+	app.layout = html.Div(
+	[
 
-    html.Div([html.Button("Download csv", id="btn"), dcc.Download(id="download")]),
-    
-     html.Div(
-        [
-        dcc.DatePickerRange(
-            id="my-date-picker-range",
-            min_date_allowed=dt(2019, 1, 1),
-            max_date_allowed=dt(2019, 1, 4),
-            initial_visible_month=dt(2019, 1, 1),
-            end_date=dt(2019, 1, 4),
-        ),
+	html.Div([html.Button("Download csv", id="btn"), dcc.Download(id="download")]),
+	
+	 html.Div(
+		[
+		dcc.DatePickerRange(
+			id="my-date-picker-range",
+			min_date_allowed=dt(2019, 1, 1),
+			max_date_allowed=dt(2019, 1, 4),
+			initial_visible_month=dt(2019, 1, 1),
+			end_date=dt(2019, 1, 4),
+		),
 
 
-        dash_table.DataTable(
-            id="datatable-interactivity",
-            columns=[
-                {
-                    "name": i,
-                    "id": i,
-                    "deletable": True,
-                    "selectable": True,
-                    "hideable": True,
-                }
-                if i == "iso_alpha3" or i == "year" or i == "id"
-                else {"name": i, "id": i, "deletable": True, "selectable": True}
-                for i in df.columns
-            ],
-            data=df.to_dict("records"),  # the contents of the table
-            editable=True,  # allow editing of data inside all cells
-            filter_action="native",  # allow filtering of data by user ('native') or not ('none')
-            sort_action="native",  # enables data to be sorted per-column by user or not ('none')
-            sort_mode="single",  # sort across 'multi' or 'single' columns
-            column_selectable="multi",  # allow users to select 'multi' or 'single' columns
-            row_selectable="multi",  # allow users to select 'multi' or 'single' rows
-            row_deletable=True,  # choose if user can delete a row (True) or not (False)
-            selected_columns=[],  # ids of columns that user selects
-            selected_rows=[],  # indices of rows that user selects
-            page_action="native",  # all data is passed to the table up-front or not ('none')
-            page_current=0,  # page number that user is on
-            page_size=6,  # number of rows visible per page
-            style_cell={  # ensure adequate header width when text is shorter than cell's text
-                "minWidth": 95,
-                "maxWidth": 95,
-                "width": 95,
-            },
-            style_cell_conditional=[  # align text columns to left. By default they are aligned to right
-                {"if": {"column_id": c}, "textAlign": "left"}
-                for c in ["country", "iso_alpha3"]
-            ],
-            style_data={  # overflow cells' content into multiple lines
-                "whiteSpace": "normal",
-                "height": "auto",
-            },
-        ),
-        ])
-    ]
+		dash_table.DataTable(
+			id="datatable-interactivity",
+			columns=[
+				{
+					"name": i,
+					"id": i,
+					"deletable": True,
+					"selectable": True,
+					"hideable": True,
+				}
+				if i == "iso_alpha3" or i == "year" or i == "id"
+				else {"name": i, "id": i, "deletable": True, "selectable": True}
+				for i in df1.columns
+			],
+			data=df1.to_dict("records"),  # the contents of the table
+			editable=True,  # allow editing of data inside all cells
+			filter_action="native",  # allow filtering of data by user ('native') or not ('none')
+			sort_action="native",  # enables data to be sorted per-column by user or not ('none')
+			sort_mode="single",  # sort across 'multi' or 'single' columns
+			column_selectable="multi",  # allow users to select 'multi' or 'single' columns
+			row_selectable="multi",  # allow users to select 'multi' or 'single' rows
+			row_deletable=True,  # choose if user can delete a row (True) or not (False)
+			selected_columns=[],  # ids of columns that user selects
+			selected_rows=[],  # indices of rows that user selects
+			page_action="native",  # all data is passed to the table up-front or not ('none')
+			page_current=0,  # page number that user is on
+			page_size=6,  # number of rows visible per page
+			style_cell={  # ensure adequate header width when text is shorter than cell's text
+				"minWidth": 95,
+				"maxWidth": 95,
+				"width": 95,
+			},
+			style_cell_conditional=[  # align text columns to left. By default they are aligned to right
+				{"if": {"column_id": c}, "textAlign": "left"}
+				for c in ["country", "iso_alpha3"]
+			],
+			style_data={  # overflow cells' content into multiple lines
+				"whiteSpace": "normal",
+				"height": "auto",
+			},
+		),
+		])
+	]
 	)
 
-    @app.callback(Output("download", "data"), [Input("btn", "n_clicks")],prevent_initial_call=True,)
-    def generate_csv(n_nlicks):
-        return send_data_frame(df.to_csv, filename=str(round(time.time()))+'.csv')
+	@app.callback(Output("download", "data"), [Input("btn", "n_clicks")],prevent_initial_call=True,)
+	def generate_csv(n_nlicks):
+		return send_data_frame(df1.to_csv, filename=str(round(time.time()))+'.csv')
 
 	def date_string_to_date(date_string):
-   		return pd.to_datetime(date_string, infer_datetime_format=True)
+		return pd.to_datetime(date_string, infer_datetime_format=True)
 
 
 	@app.callback(
-    	dash.dependencies.Output("datatable-interactivity", "data"),
-    [
-        dash.dependencies.Input("my-date-picker-range", "start_date"),
-        dash.dependencies.Input("my-date-picker-range", "end_date"),
-    ],
+		dash.dependencies.Output("datatable-interactivity", "data"),
+	[
+		dash.dependencies.Input("my-date-picker-range", "start_date"),
+		dash.dependencies.Input("my-date-picker-range", "end_date"),
+	],
 	)
 
 	def update_data(start_date, end_date):
-	    data = df1.to_dict("records")
-	    if start_date and end_date:
-	        mask = (date_string_to_date(df1["Date"]) >= date_string_to_date(start_date)) & (
-	            date_string_to_date(df1["Date"]) <= date_string_to_date(end_date)
-	        )
-	        data = df1.loc[mask].to_dict("records")
-	    return data
+		data = df1.to_dict("records")
+		if start_date and end_date:
+			mask = (date_string_to_date(df1["Date"]) >= date_string_to_date(start_date)) & (
+				date_string_to_date(df1["Date"]) <= date_string_to_date(end_date)
+			)
+			data = df1.loc[mask].to_dict("records")
+		return data
 
 	if __name__ == '__main__':
-	    app.run_server(8052, debug=True)
+		app.run_server(8052, debug=True)
